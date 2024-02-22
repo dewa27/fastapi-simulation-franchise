4ad32a291a3dcd3962ca675761d58ad204735ada
Dewa Krishna <dewakrishnaaa22@gmail.com>
2024-02-23 01:23:41 +0700

    fix main py

--- main.py
+++ main.py
@@ -44,18 +44,29 @@ test_user = {
 
 app = FastAPI()
 
-origins = {
-    "http://localhost",
-    "http://localhost:5173"
-    "*",
-}
+# origins = {
+#     "http://localhost",
+#     "http://localhost:5173"
+#     "*",
+# }
+
+
+# app.add_middleware(
+#    CORSMiddleware,
+#     allow_origins = origins,
+#     allow_credentials =True,
+#     allow_methods = ["*"],
+#     allow_headers= ["*"],
+# )
+
+origins = ["*"]
 
 app.add_middleware(
-   CORSMiddleware,
-    allow_origins = origins,
-    allow_credentials =True,
-    allow_methods = ["*"],
-    allow_headers= ["*"],
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
